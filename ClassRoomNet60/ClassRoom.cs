6b27a8237a09852e98390c233cf8268ae3e3bb92
Kasper Pelle Jensen <kaj007@edu.zealand.dk>
2025-02-06 13:23:42 +0100

    Oprettet ClassRoom klasse, samt instansieret en ny ClassRoom og tilføjet 3 nye elever

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class ClassRoom
+    {
+        public string Name { get; set; }
+        public List<Student> Students { get; set; }
+        public DateTime StartOfSemester { get; set; }
+
+        public ClassRoom() 
+        { 
+            Students = new List<Student>();
+        }
+    }
+}
--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassRoomNet60;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        ClassRoom c = new ClassRoom();
+        c.Name = "3B";
+        c.StartOfSemester = new DateTime(2024,9,1);
+        c.Students.Add(new Student("Nikolaj", 12, 8));
+        c.Students.Add(new Student("Dølle", 3, 12));
+        c.Students.Add(new Student("Kasper", 8, 4));
+
+        foreach (var student in c.Students)
+        {
+            Console.WriteLine(student.Name);
+        }
+    }
+}
