50015be5537aebeca2d671056f48009c9fbdd4f4
Kasper Pelle Jensen <kaj007@edu.zealand.dk>
2025-02-06 13:12:23 +0100

    Student added with properties and constructor

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Student
+    {
+        public string Name { get; private set; }
+        public int MonthOfBirth { get; private set; }
+        public int Birthday { get; private set; }
+
+        public Student(string name, int monthOfBirth, int birthDay) 
+        { 
+            Name = name;
+            MonthOfBirth = monthOfBirth;
+            Birthday = birthDay;
+        }
+    }
+}
